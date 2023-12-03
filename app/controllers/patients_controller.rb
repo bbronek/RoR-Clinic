d8f575130d21bbabe5d301bfd69f7d0f3c47207a
bbronek <barbro214@gmail.com>
2023-12-04 00:29:12 +0100

    Implement index and show patients actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,7 +1,13 @@
 class PatientsController < ApplicationController
   def index
+    @patients = Patient.all
+
+    @q = @patients.ransack(params[:q])
+    @patients = @q.result(distinct: true).order(:last_name).page(params[:page])
   end
 
   def show
+    @patient = Patient.find(params[:id])
+    @appointments = @patient.appointments
   end
 end
