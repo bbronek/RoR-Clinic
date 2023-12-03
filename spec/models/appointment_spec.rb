a391167566d17e2b6bed3ee743d977cbb2390837
bbronek <barbro214@gmail.com>
2023-12-03 22:29:15 +0100

    Add appointment model spec

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "requires appointment_date to be present" do
+      appointment = Appointment.new(price: 150.0, doctor: Doctor.new)
+      appointment.valid?
+      expect(appointment.errors[:appointment_date]).to include("can't be blank")
+    end
+
+    it "requires price to be present" do
+      appointment = Appointment.new(appointment_date: Date.today, doctor: Doctor.new)
+      appointment.valid?
+      expect(appointment.errors[:price]).to include("can't be blank")
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a doctor" do
+      appointment = Appointment.new
+      expect(appointment).to respond_to(:doctor)
+    end
+  end
 end
