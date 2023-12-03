a02f2c420aa4817709d81d09f805ae5b0543c54c
bbronek <barbro214@gmail.com>
2023-12-03 20:15:10 +0100

    Add doctor migration

---
+++ db/migrate/20231203190931_create_doctors.rb
@@ -0,0 +1,11 @@
+class CreateDoctors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :doctors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :specialization
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_185343) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_03_190931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "specialization"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
