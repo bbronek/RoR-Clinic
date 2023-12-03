3e7fa111f9ec821341cd01f5c23a1e4ac16bb647
bbronek <barbro214@gmail.com>
2023-12-03 23:52:55 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'appointments/index'
+  get 'appointments/new'
+  get 'appointments/create'
+  get 'appointments/edit'
+  get 'appointments/update'
+  get 'appointments/destroy'
   get 'patients/index'
   get 'patients/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
