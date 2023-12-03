ffb67ac25429c9f183acd2b99f607cd95b380333
bbronek <barbro214@gmail.com>
2023-12-04 00:29:35 +0100

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'appointments/index'
-  get 'appointments/new'
-  get 'appointments/create'
-  get 'appointments/edit'
-  get 'appointments/update'
-  get 'appointments/destroy'
-  get 'patients/index'
-  get 'patients/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :appointments
+  resources :patients
 end
