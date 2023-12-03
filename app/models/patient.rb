d8f6c0f171f38bf310ec93e3f9f9c468ab87e976
bbronek <barbro214@gmail.com>
2023-12-04 00:29:26 +0100

    Configure ransack on patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,12 @@
 class Patient < ApplicationRecord
   has_many :appointments
   validates_presence_of :first_name, :last_name, :gender, :pesel, :date_of_birth
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[gender last_name]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[appointments]
+  end
 end
