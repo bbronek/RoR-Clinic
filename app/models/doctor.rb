6a27850da64a114651252b594c008f8e50181ffe
bbronek <barbro214@gmail.com>
2023-12-04 01:43:46 +0100

    Add full name method to the doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   validates_presence_of :first_name, :last_name, :specialization
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
