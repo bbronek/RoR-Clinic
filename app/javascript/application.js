8eac2585a0b10f5365e5a45a46203f58d03e93ee
bbronek <barbro214@gmail.com>
2023-12-04 02:34:01 +0100

    Require chartkick in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require Chart.bundle
+
 import "@hotwired/turbo-rails"
 import "controllers"
