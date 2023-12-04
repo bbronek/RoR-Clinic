1540a2363aac42223742cf1f7bc6ea5c3911a1c2
bbronek <barbro214@gmail.com>
2023-12-04 02:42:49 +0100

    Fix routes Remove get 'welcome/index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   root "welcome#index"
 
   resources :appointments
   resources :patients do
     resources :appointments, only: [:index], controller: "patients/appointments"
   end
+
+  get "statistics/patient_births"
 end
