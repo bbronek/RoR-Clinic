0e3823efce4143b630a79f36b4072e8f8c2d066b
bbronek <barbro214@gmail.com>
2023-12-04 02:19:19 +0100

    Add a root dummy page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root "welcome#index"
+
   resources :appointments
   resources :patients do
     resources :appointments, only: [:index], controller: "patients/appointments"
